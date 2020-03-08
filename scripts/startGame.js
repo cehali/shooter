936c4505488eeb8dc104a221fb219a6bb71e627c
Cezary Haliniarz <cezary.haliniarz@gmail.com>
2020-03-08 21:56:01 +0100

    Delete enemy if hit

--- scripts/startGame.js
+++ scripts/startGame.js
@@ -3,6 +3,7 @@ let context;
 function startGame() {
   gameBoardActions.create();
   heroActions.create();
+  gameBoard.enemies.push(new Enemy());
   sightActions.create();
   updateGameArea();
 };
@@ -11,8 +12,9 @@ function updateGameArea() {
   requestAnimationFrame(() => {
     gameBoardActions.update();
     heroActions.update();
+    gameBoard.enemies.map(enemy => enemy.update());
     sightActions.update();
-    gameBoard.shotsFired.map(shot => shot.update());
+    gameBoard.shotsFired.map((shot, index) => shot.update(index));
     updateGameArea();
   });
 };
@@ -21,6 +23,7 @@ const gameBoard = {
   width: 1000,
   height: 700,
   keysClicked: [],
+  enemies: [],
   shotsFired: []
 };
 
@@ -92,26 +95,63 @@ const sightActions = {
 
 class Shot {
   constructor() {
-    this.x = hero.x + hero.width / 2;
-    this.y = hero.y + hero.height / 2;
     this.width = 5;
     this.height = 5;
+    this.x = hero.x + (hero.width / 2) - (this.height / 2);
+    this.y = hero.y + (hero.height / 2) - (this.height / 2);
     this.color = 'white';
     const speedPerTick = 5;
-    const deltaX = sight.x - hero.x;
-    const deltaY = sight.y - hero.y;
+    const deltaX = sight.x + (sight.width / 2) - hero.x - (hero.width / 2);
+    const deltaY = sight.y + (sight.height / 2) - hero.y - (hero.height / 2);
     const distance = Math.sqrt((deltaX * deltaX) + (deltaY * deltaY));
     const ratio = speedPerTick / distance;
     this.movementX = ratio * deltaX;
     this.movementY = ratio * deltaY;
   }
 
-  update() {
+  update(index) {
     this.x += this.movementX;
     this.y += this.movementY;
+    const enemiesShot = gameBoard.enemies.filter((enemy, enemyIndex) => {
+      if (
+        this.y > enemy.y + enemy.height ||
+        this.y + this.height < enemy.y ||
+        this.x > enemy.x + enemy.width ||
+        this.x + this.width < enemy.x
+      ) {
+        return false;
+      } 
+      gameBoard.enemies.splice(enemyIndex, 1);
+      return true;
+    });
+    if (enemiesShot.length === 0) {
+      context.fillStyle = this.color
+      context.fillRect(this.x, this.y, this.width, this.height);
+    } else {
+      gameBoard.shotsFired.splice(index, 1);
+    }
+  }
+};
+
+class Enemy {
+  constructor() {
+    this.x = 300;
+    this.y = 300;
+    this.width = 30;
+    this.height = 30;
+    this.color = 'blue';
+    this.create();
+  }
+
+  create() {
     context.fillStyle = this.color
     context.fillRect(this.x, this.y, this.width, this.height);
-  }
+  };
+
+  update() {
+    context.fillStyle = this.color
+    context.fillRect(this.x, this.y, this.width, this.height);
+  };
 };
 
 window.addEventListener('keydown', (event) => {
