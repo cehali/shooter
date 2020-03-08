bce8f5c3b73a9379a2c46f7c0e59bc88d80263a0
Cezary Haliniarz <cezary.haliniarz@gmail.com>
2020-03-08 21:55:37 +0100

    Add shooting functionality

--- scripts/startGame.js
+++ scripts/startGame.js
@@ -2,30 +2,43 @@ let context;
 
 function startGame() {
   gameBoardActions.create();
-  gamePieceActions.create();
+  heroActions.create();
+  sightActions.create();
   updateGameArea();
 };
 
+function updateGameArea() {
+  requestAnimationFrame(() => {
+    gameBoardActions.update();
+    heroActions.update();
+    sightActions.update();
+    gameBoard.shotsFired.map(shot => shot.update());
+    updateGameArea();
+  });
+};
+
 const gameBoard = {
-  width: 480,
-  height: 270,
-  keysClicked: []
-}
+  width: 1000,
+  height: 700,
+  keysClicked: [],
+  shotsFired: []
+};
 
 const gameBoardActions = {
   create: () => {
     const canvas = document.createElement('canvas');
     canvas.width = gameBoard.width;
     canvas.height = gameBoard.height;
+    canvas.style.cursor = 'none';
     context = canvas.getContext('2d');
     document.body.insertBefore(canvas, document.body.childNodes[0]);
   },
   update: () => {
     context.clearRect(0, 0, gameBoard.width, gameBoard.height);
   }
-}
+};
 
-const gamePiece = {
+const hero = {
   x: 10,
   y: 120,
   width: 30,
@@ -35,40 +48,87 @@ const gamePiece = {
   translationY: 0
 };
 
-const gamePieceActions = {
+const heroActions = {
   create: () => {
-    const {x, y, width, height, color} = gamePiece;
+    const {x, y, width, height, color} = hero;
     context.fillStyle = color
     context.fillRect(x, y, width, height);
   },
   update: () => {
-    const {width, height, color} = gamePiece;
-    if (gameBoard.keysClicked && gameBoard.keysClicked['KeyA']) gamePiece.translationX += -2;
-    if (gameBoard.keysClicked && gameBoard.keysClicked['KeyDw']) gamePiece.translationX += 2;
-    if (gameBoard.keysClicked && gameBoard.keysClicked['KeyW']) gamePiece.translationY += -2;
-    if (gameBoard.keysClicked && gameBoard.keysClicked['KeyS']) gamePiece.translationY += 2;
-    gamePiece.x += gamePiece.translationX;
-    gamePiece.y += gamePiece.translationY;
+    const {width, height, color} = hero;
+    if (gameBoard.keysClicked && gameBoard.keysClicked['KeyA']) hero.translationX += -2;
+    if (gameBoard.keysClicked && gameBoard.keysClicked['KeyD']) hero.translationX += 2;
+    if (gameBoard.keysClicked && gameBoard.keysClicked['KeyW']) hero.translationY += -2;
+    if (gameBoard.keysClicked && gameBoard.keysClicked['KeyS']) hero.translationY += 2;
+    hero.x += hero.translationX;
+    hero.y += hero.translationY;
     context.fillStyle = color
-    context.fillRect(gamePiece.x, gamePiece.y, width, height);
-    gamePiece.translationX = 0;
-    gamePiece.translationY = 0;
+    context.fillRect(hero.x, hero.y, width, height);
+    hero.translationX = 0;
+    hero.translationY = 0;
   }
-}
+};
 
-function updateGameArea() {
-  requestAnimationFrame(() => {
-    gameBoardActions.update();
-    gamePieceActions.update();
-    updateGameArea();
-  });
-}
+const sight = {
+  x: 80,
+  y: 130,
+  width: 10,
+  height: 10,
+  color: 'green'
+};
 
-window.addEventListener('keydown', function (event) {
+const sightActions = {
+  create: () => {
+    const {x, y, width, height, color} = sight;
+    context.fillStyle = color
+    context.fillRect(x, y, width, height);
+  },
+  update: () => {
+    const {x, y, width, height, color} = sight;
+    context.fillStyle = color
+    context.fillRect(x, y, width, height);
+  }
+};
+
+class Shot {
+  constructor() {
+    this.x = hero.x + hero.width / 2;
+    this.y = hero.y + hero.height / 2;
+    this.width = 5;
+    this.height = 5;
+    this.color = 'white';
+    const speedPerTick = 5;
+    const deltaX = sight.x - hero.x;
+    const deltaY = sight.y - hero.y;
+    const distance = Math.sqrt((deltaX * deltaX) + (deltaY * deltaY));
+    const ratio = speedPerTick / distance;
+    this.movementX = ratio * deltaX;
+    this.movementY = ratio * deltaY;
+  }
+
+  update() {
+    this.x += this.movementX;
+    this.y += this.movementY;
+    context.fillStyle = this.color
+    context.fillRect(this.x, this.y, this.width, this.height);
+  }
+};
+
+window.addEventListener('keydown', (event) => {
   gameBoard.keysClicked = (gameBoard.keysClicked || []);
   gameBoard.keysClicked[event.code] = (event.type == "keydown");
 });
 
-window.addEventListener('keyup', function (event) {
+window.addEventListener('keyup', (event) => {
   gameBoard.keysClicked[event.code] = (event.type == "keydown");
+});
+
+window.addEventListener('mousemove', (event) => {
+  sight.x = event.pageX;
+  sight.y = event.pageY;
+});
+
+window.addEventListener('mousedown', (event) => {
+  const shot = new Shot();
+  gameBoard.shotsFired.push(shot);
 });
